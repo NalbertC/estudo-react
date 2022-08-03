e9d6c7d6dd74552989e1d57c216eb5ee1e7c9fdd
NalbertC <nalberthcastro1510@gmail.com>
2022-08-03 20:38:55 -0300

    trabalhando com estados

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,17 +1,25 @@
+import React, { useState } from 'react'
+
 import './style.css'
 
 import { Card } from '../../components/Card'
 
 export function Home() {
+  const [studentName, setStudentName] = useState()
+
   return (
     <div className='container'>
-      <h1>Lista de presença</h1>
-      <input type="text" placeholder="Digite o nome ..." />
+      <h1>Nome : {studentName} </h1>
+      <input
+        type="text"
+        placeholder="Digite o nome ..."
+        onChange={e => setStudentName(e.target.value)}
+      />
       <button type="button">Adicionar</button>
 
       <Card name="Nalberth Castro" time="12:34:23" />
-      <Card name="Vitória Peres" time="09:32:09"/>
-      <Card name="Anna Maria" time="08:45:41"/>
+      <Card name="Vitória Peres" time="09:32:09" />
+      <Card name="Anna Maria" time="08:45:41" />
     </div>
   )
 }
